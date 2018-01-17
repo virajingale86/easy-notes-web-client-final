47771c3e74429f53675b06692419cae227859894
vf-root <vf-root@vfroot-ThinkPad-T430s>
2018-01-18 02:43:02 +0530

    Removed Load Balanced

--- src/main/java/com/example/easynotes/EasyNotesWebApplication.java
+++ src/main/java/com/example/easynotes/EasyNotesWebApplication.java
@@ -2,8 +2,6 @@ package com.example.easynotes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -22,7 +20,6 @@ public class EasyNotesWebApplication {
 	
 
 	@Bean
-	@LoadBalanced
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
